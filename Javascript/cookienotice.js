3ab4a6a23738f78711fe59692cdb8517bdf5e03d
Evan Boehs <ebrobot2000@gmail.com>
2021-02-03 08:08:07 -0500

    beautifying

--- Javascript/cookienotice.js
+++ Javascript/cookienotice.js
@@ -1,18 +1,17 @@
 if (Cookies.get("_cookienotice") == "Disabled") {
-        document.getElementById("hideablecookiebanner").style.display = "none";
-        console.log("Cookie notice is hidden. use cookieshow() to enable");
+    document.getElementById("hideablecookiebanner").style.display = "none";
+    console.log("Cookie notice is hidden. use cookieshow() to enable");
 
-}
-else {
-        document.getElementById("hideablecookiebanner").style.display = "block";
-        console.log("Cookie notice is not. use cookiehide() to disable");
+} else {
+    document.getElementById("hideablecookiebanner").style.display = "block";
+    console.log("Cookie notice is not. use cookiehide() to disable");
 }
 
 function cookiehide() {
-        Cookies.set('_cookienotice', 'Disabled');
-        document.getElementById("hideablecookiebanner").style.display = "none";
-        console.log("hid for the first time");
-    };
+    Cookies.set('_cookienotice', 'Disabled');
+    document.getElementById("hideablecookiebanner").style.display = "none";
+    console.log("hid for the first time");
+};
 
 function cookieshow() {
     Cookies.set('_cookienotice', 'Enabled');
