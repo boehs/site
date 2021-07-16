8303aee037aceb4e44525abbddd6f41c3e8a0f27
Evan Boehs <51836263+boehs@users.noreply.github.com>
2021-07-15 20:12:24 -0400

    phase out cookies

--- src/Javascript/cookienotice.js
+++ src/Javascript/cookienotice.js
@@ -1,20 +1,16 @@
-if (Cookies.get("_cookienotice") == "Disabled") {
+if (localStorage.getItem("noticeis") == "Disabled") {
     document.getElementById("hideablecookiebanner").style.display = "none";
-    console.log("Cookie notice is hidden. use cookieshow() to enable");
 
 } else {
     document.getElementById("hideablecookiebanner").style.display = "block";
-    console.log("Cookie notice is not. use cookiehide() to disable");
 }
 
 function cookiehide() {
-    Cookies.set('_cookienotice', 'Disabled');
+    localStorage.setItem("noticeis", "Disabled");
     document.getElementById("hideablecookiebanner").style.display = "none";
-    console.log("hid for the first time");
 };
 
 function cookieshow() {
-    Cookies.set('_cookienotice', 'Enabled');
-    document.getElementById("hideablecookiebanner").style.display = "";
-    console.log("unhid");
+    localStorage.setItem("noticeis", "Enabled");
+    document.getElementById("hideablecookiebanner").style.display = "block";
 };
--- src/script.js
+++ src/script.js
@@ -2,13 +2,8 @@ function shuffle(array) {
 	var m = array.length,
 		t,
 		i;
-
-	// While there remain elements to shuffle…
 	while (m) {
-		// Pick a remaining element…
 		i = Math.floor(Math.random() * m--);
-
-		// And swap it with the current element.
 		t = array[m];
 		array[m] = array[i];
 		array[i] = t;
@@ -17,27 +12,22 @@ function shuffle(array) {
 	return array;
 }
 
-var Index = 0;
-var x = document.getElementsByClassName("imageslide");
-x = Array.from(x);
-console.log(x)
-x = shuffle(x);
-console.log(x);
+var index = 0;
+var x = shuffle(Array.from(document.getElementsByClassName("imageslide")));
 carousel();
 
 function carousel() {
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
-	Index++;
-	if (Index > x.length) {
-		console.log(x.length);
-		Index = 1;
+	index++;
+	if (index > x.length) {
+		index = 1;
 	}
-	x[Index - 1].style.display = "block";
+	x[index - 1].style.display = "block";
 	setTimeout(carousel, 5000);
 }
 
 document.onmousemove = function (event) {
-  document.documentElement.style.cssText = `--deg: ${180 + Math.round((event.clientX-window.innerWidth/2)/18)}deg;`
+  document.documentElement.style.cssText = `--deg: ${180 - (Math.round((event.clientX-window.innerWidth/2)/18))}deg;`
 };
