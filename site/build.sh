5922e82cca2d6c892da0e56436085070dfc5e180
Evan Boehs <evan@boehs.org>
2022-01-03 18:00:39 -0500

    build script improvements

--- site/build.sh
+++ site/build.sh
@@ -14,13 +14,15 @@ for f in $(find * -name '*.html' -or -name ''); do
     declare matchedtag=$(echo $contents | pcregrep -o1 -e 'header class="(astro-.*?)"')
     echo "for what it's worth, the matched tag was $matchedtag"
     # Remove new lines
-    # echo -n $(tr -d "\n" < $f) > $f
+    if ! grep -q "script" "$f"; then
+        echo -n $(tr -d "\n" < $f) > $f
+    fi
     # Remove all astro prefixes
-    sed -Ei "s/( class=\"$matchedtag\"| $matchedtag)//g;s/astro-/e/g" $f
+    sed -Ei "s/( class=\"$matchedtag\"| $matchedtag)//g;s/astro-/e/g;/^[[:space:]]*$/d" $f
     ### Time for CSS ###
     # Remove all astro prefixes
     echo "parsing css for file $i"
-    sed -Ei "s/>\.$matchedtag/>\*/g;s/.$matchedtag//g;s/astro-/e/g" $matchedcss
+    sed -Ei "s/>\.$matchedtag/>\*/g;s/.$matchedtag//g;s/astro-/e/g" assets/$(basename $matchedcss)
     i=$(($i + 1))
 done
 # Move to site root
