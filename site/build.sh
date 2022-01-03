23a785d15ed2bf5ff3aac851b0e7a845586bdc26
Evan Boehs <evan@boehs.org>
2022-01-02 21:34:18 -0500

    silly evan did a silly bug

--- site/build.sh
+++ site/build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+#npm install -G pnpm
 #pnpm install
 astro build
 cd dist
 declare i=1
 for f in $(find * -name '*.html' -or -name ''); do
     echo "parsing file: $f, this is file $i"
-    i=$(($i + 1))
     # File contents
     declare contents=$(cat $f)
-    # Find css file names
+    # css file name
     declare matchedcss=$(echo $contents | pcregrep -o1 -e '<link rel="stylesheet" href="(.*\/(.*)\.css)')
-    # Find standard astro prefix
+    # standard astro prefix
     declare matchedtag=$(echo $contents | pcregrep -o1 -e 'header class="(astro-.*?)"')
+    echo "for what it's worth, the matched tag was $matchedtag"
     # Remove new lines
     # echo -n $(tr -d "\n" < $f) > $f
     # Remove all astro prefixes
@@ -19,8 +20,13 @@ for f in $(find * -name '*.html' -or -name ''); do
     ### Time for CSS ###
     # Remove all astro prefixes
     echo "parsing css for file $i"
-    sed -Ei "s/.$matchedtag//g;s/astro-//g" $matchedcss
+    sed -Ei "s/>\.$matchedtag/>\*/g;s/.$matchedtag//g;s/astro-//g" $matchedcss
+    i=$(($i + 1))
 done
 # Move to site root
 # MV fails on populated directory
-cp -r ./* ../../ && rm -r ./*
+if [[ $1 = 'prod' ]]
+then
+    echo 'is in prod, moving files to root'
+    rm -r ../../assets/ && cp -r ./* ../../ && rm -r ./*
+fi
