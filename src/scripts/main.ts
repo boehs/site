6b3af0277e295ec1fd5d4b2f69d8c43459169f74
Evan Boehs <evan@boehs.org>
2022-02-21 21:49:25 -0500

    make slightly smaller

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,26 +6,25 @@ Object.values(flowerpower.split('\n?'))
     .map((step: string,i) => {
         if (i == 0) prev = step.split('\n')
         flowers[0].push(Number(step.substring(0, 1)))
-        flowers[1].push((() => {
-            return step.substring(1).split('\n').map((x,i2) => {
-                x = x.replace(/[0-9]/g, match => " ".repeat(Number(match) + 2))
-                if (x == '!') return prev[i2]
-                else return x
-            }).join('\n')
-        })());
+        flowers[1].push((() =>
+            step.substring(1).split('\n').map((x,i2) =>
+                x.replace(/[0-9]/g, match => " ".repeat(Number(match) + 2))
+                .replace(/!/g,prev[i2])
+            ).join('\n')
+        )());
         prev = flowers[1][i].split('\n')
     })
 flowers = flowers.map(x => x.reverse())
 
-let isanimating = false
-let timeleft;
-let ishover = true;
-let state = false;
-let flowerElm = document.getElementById('flower')
+let isanimating = false,
+timeleft,
+ishover = true,
+state = false,
+flowerElm = document.getElementById('flower')
 
 function doanimation(should_reverse = false) {
-    let flower_time = [...flowers[0]]
-    let flower = [...flowers[1]]
+    let flower_time = [...flowers[0]],
+    flower = [...flowers[1]]
     isanimating = true;
     if (should_reverse) {
         flower_time = flower_time.reverse()
@@ -54,7 +53,7 @@ function shit() {
 
 timeleft = flowers[0].length
 
-// Hydrate via script to ensure it does not look wierd for those without javascript
+// Hydrate via script to ensure it does not look wack for those without javascript
 flowerElm.innerHTML = flowers[1][flowers[0].length - 1]
 flowerElm.onmouseover = () => {
     ishover = true
