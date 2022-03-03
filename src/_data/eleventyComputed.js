9aa86562ed91513654c4a5f9f9e691f62aad969c
Evan Boehs <evan@boehs.org>
2022-03-03 00:46:07 -0500

    return color by default in 11ty computed

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,15 +1,17 @@
 module.exports = {
-    title(data) {
-        if (data['no-title']) return
-        if (data.title) return data.title
-        return data.page.fileSlug.replace('-',' ')
-    },
-    color(data) {
-        if (!data.tags) return
-        for(tag of data.tags) {
-            if (!data.tagList[tag]) continue
-            if (data.tagList[tag].color) return data.tagList[tag].color
-            else continue
-        }
+  title(data) {
+    if (data["no-title"]) return;
+    if (data.title) return data.title;
+    return data.page.fileSlug.replace("-", " ");
+  },
+  color(data) {
+    if (data.tags) {
+      for (tag of data.tags) {
+        if (!data.tagList[tag]) continue;
+        if (data.tagList[tag].color) return data.tagList[tag].color;
+        else continue;
+      }
     }
-}
+    return data.config.color
+  }
+};
