f892a1a6a533655a4afe07f78b6824b2cd5b7d80
Evan Boehs <evan@boehs.org>
2022-03-18 19:25:59 -0400

    format file

--- src/pages/garden/garden.11tydata.js
+++ src/pages/garden/garden.11tydata.js
@@ -1,56 +1,66 @@
 // This regex finds all wikilinks in a string
-const wikilinkRegExp = /\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g
+const wikilinkRegExp = /\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g;
 
 function caselessCompare(a, b) {
-    return a.toLowerCase() === b.toLowerCase();
+  return a.toLowerCase() === b.toLowerCase();
 }
 
 module.exports = {
-    layout: "post.njk",
-    in: "garden",
-    hasCodeBlock: true,
-    permalink: "{{ page.filePathStem | dropContentFolder: \"pages/garden\" }}.html",
-    eleventyComputed: {
-        backlinks: (data) => {
-            const notes = data.collections.all;
-            const currentFileSlug = data.page.filePathStem.replace('/pages/garden/node/', '');
-            let backlinks = [];
+  layout: "post.njk",
+  in: "garden",
+  hasCodeBlock: true,
+  permalink: '{{ page.filePathStem | dropContentFolder: "pages/garden" }}.html',
+  eleventyComputed: {
+    backlinks: (data) => {
+      const notes = data.collections.all;
+      const currentFileSlug = data.page.filePathStem.replace(
+        "/pages/garden/node/",
+        ""
+      );
+      let backlinks = [];
 
-            data.internal.exists?.add(currentFileSlug.toLowerCase())
-            
-            // Search the other notes for backlinks
-            for(const otherNote of notes) {
-                const noteContent = otherNote.template.frontMatter.content;
-                // Get all links from otherNote
-                
-                const outboundLinks = (function() {switch (otherNote.data.links) {
-                    case undefined: {
-                        const links = (noteContent.match(wikilinkRegExp) || [])
-                        .map(link => (
-                            // Extract link location
-                            link.slice(2,-2)
-                                .split("|")[0]
-                                .replace(/.(md|markdown)\s?$/i, "")
-                                .trim()
-                        ));
-                        otherNote.data.links = links
-                        return links
-                    }
-                    default: return otherNote.data.links
-                }})()
-                // If the other note links here, return related info
-                if(outboundLinks.some(link => caselessCompare(link, currentFileSlug))) {
-                    backlinks.push({
-                        url: otherNote.url,
-                        title: otherNote.data.title,
-                    })
-                }
-                
-                outboundLinks.forEach(link => {
-                    if (!data.internal.exists.has(link.toLowerCase())) data.internal.four.add(link)
-                })
+      data.internal.exists?.add(currentFileSlug.toLowerCase());
+
+      // Search the other notes for backlinks
+      for (const otherNote of notes) {
+        const noteContent = otherNote.template.frontMatter.content;
+        // Get all links from otherNote
+
+        const outboundLinks = (function () {
+          switch (otherNote.data.links) {
+            case undefined: {
+              const links = (noteContent.match(wikilinkRegExp) || []).map(
+                (link) =>
+                  // Extract link location
+                  link
+                    .slice(2, -2)
+                    .split("|")[0]
+                    .replace(/.(md|markdown)\s?$/i, "")
+                    .trim()
+              );
+              otherNote.data.links = links;
+              return links;
             }
-            return backlinks;
+            default:
+              return otherNote.data.links;
+          }
+        })();
+        // If the other note links here, return related info
+        if (
+          outboundLinks.some((link) => caselessCompare(link, currentFileSlug))
+        ) {
+          backlinks.push({
+            url: otherNote.url,
+            title: otherNote.data.title,
+          });
         }
-    }
-}
+
+        outboundLinks.forEach((link) => {
+          if (!data.internal.exists.has(link.toLowerCase()))
+            data.internal.four.add(link);
+        });
+      }
+      return backlinks;
+    },
+  },
+};
