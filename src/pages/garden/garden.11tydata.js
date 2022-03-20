cbafbeb9aafcaa3007f45b2ac30162d752e0f621
Evan Boehs <evan@boehs.org>
2022-03-20 17:49:54 -0400

    format

--- src/pages/garden/garden.11tydata.js
+++ src/pages/garden/garden.11tydata.js
@@ -2,76 +2,81 @@
 const wikilinkRegExp = /\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g;
 
 function caselessCompare(a, b) {
-    return a.toLowerCase() === b.toLowerCase();
+  return a.toLowerCase() === b.toLowerCase();
 }
 
 function backlinksApi(data) {
-    const notes = data.collections.all;
-    const currentFileSlug = data.page.filePathStem.replace(
-        "/pages/garden/node/",
-        ""
-    );
-    let backlinks = [];
-    
-    data.internal.exists?.add(currentFileSlug.toLowerCase())
-    
-    // Search the other notes for backlinks
-    for (const otherNote of notes) {
-        const noteContent = otherNote.template.frontMatter.content;
-        const noteAsLink = otherNote.data.page.filePathStem.replace('/pages/garden/node/', '').toLowerCase()
-        
-        data.internal.exists?.add(noteAsLink)
-        
-        // Get all links from otherNote
-        const outboundLinks = (function () {
-            switch (otherNote.data.links) {
-                case undefined: {
-                    const links = (noteContent.match(wikilinkRegExp) || []).map((link) =>
-                        // Extract link location
-                        link
-                            .slice(2, -2)
-                            .split("|")[0]
-                            .replace(/.(md|markdown)\s?$/i, "")
-                            .trim()
-                    );
-                    otherNote.data.links = links;
-                    return links;
-                }
-                default:
-                    return otherNote.data.links;
-            }
-        })();
-        // If the other note links here, return related info
-        if (outboundLinks.some((link) => caselessCompare(link, currentFileSlug))) {
-            backlinks.push({
-                url: otherNote.url,
-                title: otherNote.data.title,
-            });
+  const notes = data.collections.all;
+  const currentFileSlug = data.page.filePathStem.replace(
+    "/pages/garden/node/",
+    ""
+  );
+  let backlinks = [];
+
+  data.internal.exists?.add(currentFileSlug.toLowerCase());
+
+  // Search the other notes for backlinks
+  for (const otherNote of notes) {
+    const noteContent = otherNote.template.frontMatter.content;
+    const noteAsLink = otherNote.data.page.filePathStem
+      .replace("/pages/garden/node/", "")
+      .toLowerCase();
+
+    data.internal.exists?.add(noteAsLink);
+
+    // Get all links from otherNote
+    const outboundLinks = (function () {
+      switch (otherNote.data.links) {
+        case undefined: {
+          const links = (noteContent.match(wikilinkRegExp) || []).map((link) =>
+            // Extract link location
+            link
+              .slice(2, -2)
+              .split("|")[0]
+              .replace(/.(md|markdown)\s?$/i, "")
+              .trim()
+          );
+          otherNote.data.links = links;
+          return links;
         }
-        
-        (otherNote.data.aliases || []).forEach(function(alias) {
-            data.internal.exists?.add(alias)
-        })
-                
-        outboundLinks.forEach(link => {
-            if (!data.internal.exists.has(link.toLowerCase())) data.internal.four.add(data.page.url.replace(/\/[^\/]*?(\..+)?$/, `/${link}$1`).toLowerCase())
-        })
-    }    
-    return [backlinks,data.internal.four];
+        default:
+          return otherNote.data.links;
+      }
+    })();
+    // If the other note links here, return related info
+    if (outboundLinks.some((link) => caselessCompare(link, currentFileSlug))) {
+      backlinks.push({
+        url: otherNote.url,
+        title: otherNote.data.title,
+      });
+    }
+
+    (otherNote.data.aliases || []).forEach(function (alias) {
+      data.internal.exists?.add(alias);
+    });
+
+    outboundLinks.forEach((link) => {
+      if (!data.internal.exists.has(link.toLowerCase()))
+        data.internal.four.add(
+          data.page.url.replace(/\/[^\/]*?(\..+)?$/, `/${link}$1`).toLowerCase()
+        );
+    });
+  }
+  return [backlinks, data.internal.four];
 }
 
 module.exports = {
-    layout: "post.njk",
-    in: "garden",
-    hasCodeBlock: true,
-    permalink: '{{ page.filePathStem | dropContentFolder: "pages/garden" }}.html',
-    eleventyComputed: {
-        backlinks: (data) => {
-            return backlinksApi(data)[0];
-        },
-        brokenLinks: (data,canRun = false) => {
-            if (!canRun) return 'not called from 11ty config'
-            return backlinksApi(data)[1]
-        },
+  layout: "post.njk",
+  in: "garden",
+  hasCodeBlock: true,
+  permalink: '{{ page.filePathStem | dropContentFolder: "pages/garden" }}.html',
+  eleventyComputed: {
+    backlinks: (data) => {
+      return backlinksApi(data)[0];
     },
+    brokenLinks: (data, canRun = false) => {
+      if (!canRun) return "not called from 11ty config";
+      return backlinksApi(data)[1];
+    },
+  },
 };
