4a3bb07cace8f5dab348ac24bc948cb6ce1b03b4
Evan Boehs <evan@boehs.org>
2022-12-23 09:21:25 -0500

    Not really the smallest possible but progress ayy?

--- html/_public/ts/main.ts
+++ html/_public/ts/main.ts
@@ -2,15 +2,15 @@ console.log('is anyone out there? 🔦')
 
 const flowerpower = '@@import _data/deets/flowerpower.txt'
 
-let flowers: [[number?],[string?]] = [[], []], prev: string[];
+let flowers: [[number?], [string?]] = [[], []], prev: string[];
 flowerpower.split('\n?')
-    .map((step: string,i) => {
+    .map((step: string, i) => {
         if (i == 0) prev = step.split('\n')
         flowers[0].push(Number(step.substring(0, 1)))
         flowers[1].push((() =>
-            step.split(/\n/).map((x,i2) =>
+            step.substring(1).replaceAll(/!(?<!$)/g, '!\n').split('\n').map((x, i2) =>
                 x.replace(/[0-9]/g, match => " ".repeat(Number(match) + 2))
-                .replace(/!/g,prev[i2])
+                    .replace(/!/g, prev[i2])
             ).join('\n')
         )());
         prev = flowers[1][i].split('\n')
@@ -19,17 +19,17 @@ flowerpower.split('\n?')
 flowers = flowers.map(x => x.reverse())
 
 let isanimating = false,
-timeleft: number,
-ishover = true,
-state = false,
-flowerElm = document.getElementById('flower')
+    timeleft: number,
+    ishover = true,
+    state = false,
+    flowerElm = document.getElementById('flower')
 
 // Hydrate via script to ensure it does not look wack for those without javascript
 flowerElm.innerHTML = flowers[1][flowers[0].length - 1]
 
 function doanimation(should_reverse = false) {
     let flower_time = [...flowers[0]],
-    flower = [...flowers[1]]
+        flower = [...flowers[1]]
     isanimating = true;
     if (should_reverse) {
         flower_time = flower_time.reverse()
--- utils/CompactFlowers.ts
+++ utils/CompactFlowers.ts
@@ -1,4 +1,6 @@
-const file: string = await Deno.readTextFile('../html/_data/deets/og_flowerpower.txt')
+import { readFile } from "fs/promises"
+
+const file: string = await readFile('../html/_data/deets/og_flowerpower.txt','utf8')
 const splited_file = file.split('\n?')
 
 let prev = Array.from({length: splited_file[0].split('\n').length - 1}, (_, i) => undefined)
@@ -11,11 +13,11 @@ let newFile = splited_file.map(step => {
         else return line
     })
     prev = step.split('\n')
-    return finishedSteps.join('\n')
+    return finishedSteps.join('\n').replaceAll(/!\n/g,'!')
 }).join('\n?')
 
 Array.from(Array(10).keys()).reverse().forEach(i => {
     newFile = newFile.replaceAll(" ".repeat(i+2),`${i}`)
 })
 
-await Deno.writeTextFile('../html/_data/deets/flowerpower.txt',newFile)
+await Bun.write('../html/_data/deets/flowerpower.txt',newFile)
