77697878c636069161f7f68040808f287c195276
Evan Boehs <evan@boehs.org>
2022-12-25 15:53:04 -0500

    starynight v1

--- utils/CompactFlowers.ts
+++ utils/CompactFlowers.ts
@@ -1,23 +1,24 @@
-import { readFile } from "fs/promises"
+import { readdir, readFile } from "fs/promises"
 
-const file: string = await readFile('../anim/flowerpower.txt','utf8')
-const splited_file = file.split('\n?')
+(await readdir('../anim', 'utf8')).forEach(async fileName => {
+    const file = await readFile(`../anim/${fileName}`,'utf8')
+    const splited_file = file.split('\n?')
 
-let prev = Array.from({length: splited_file[0].split('\n').length - 1}, (_, i) => undefined)
+    let prev = Array.from({ length: splited_file[0].split('\n').length - 1 }, (_, i) => undefined)
 
-let newFile = splited_file.map(step => {
+    let newFile = splited_file.map(step => {
+        const finishedSteps = step.split('\n').map((line, i) => {
+            if (i == 0) return line
+            if (prev[i] == line) return '!'
+            else return line
+        })
+        prev = step.split('\n')
+        return finishedSteps.join('\n').replaceAll(/!\n/g, '!')
+    }).join('\n?')
 
-    const finishedSteps = step.split('\n').map((line,i) => {
-        if (i == 0) return line
-        if (prev[i] == line) return '!'
-        else return line
+    Array.from(Array(10).keys()).reverse().forEach(i => {
+        newFile = newFile.replaceAll(" ".repeat(i + 2), `${i}`)
     })
-    prev = step.split('\n')
-    return finishedSteps.join('\n').replaceAll(/!\n/g,'!')
-}).join('\n?')
 
-Array.from(Array(10).keys()).reverse().forEach(i => {
-    newFile = newFile.replaceAll(" ".repeat(i+2),`${i}`)
+    await Bun.write(`../html/_data/anim/${fileName}`, newFile)
 })
-
-await Bun.write('../html/_data/deets/flowerpower.txt',newFile)
