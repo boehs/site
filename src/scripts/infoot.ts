d1015626c71ceac61d5ede7254d00f1cd8dfab08
Evan Boehs <evan@boehs.org>
2024-01-16 18:50:17 -0500

    simplify again

--- src/scripts/infoot.ts
+++ src/scripts/infoot.ts
@@ -4,37 +4,28 @@ function ordinal(n: number) {
     return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
 
-(async () => {
-    let root = "{{{this.config.analytics.base}}}/api/";
-
-    let token = (
-        await fetch(`${root}share/{{{this.config.analytics.pub}}}`).then((v) =>
-            v.json(),
-        )
-    ).token as string;
-    let res = (
-        await fetch(
-            `${root}websites/{{{this.config.analytics.id}}}/stats?` +
-                // @ts-ignore
-                new URLSearchParams({
-                    url: window.location.pathname,
-                }),
-            {
-                headers: {
-                    "X-Umami-Share-Token": token,
-                },
-            },
-        ).then(
-            (res) =>
-                res.json() as Promise<{
-                    pageviews: {
-                        value: number;
-                    };
-                }>,
-        )
-    ).pageviews.value;
-    let el = document.querySelector("article")!;
-    el.innerHTML += `<hr/><p>You are the <strong>${ordinal(
-        res + 1,
-    )}</strong> visitor to this page!</p>`;
-})();
+fetch(
+    `{{{this.config.analytics.base}}}/api/websites/{{{this.config.analytics.id}}}/stats?` +
+        new URLSearchParams({
+            url: window.location.pathname,
+        }),
+    {
+        headers: {
+            "X-Umami-Share-Token": "{{{this.config.analytics.pub}}}",
+        },
+    },
+)
+    .then(
+        (res) =>
+            res.json() as Promise<{
+                pageviews: {
+                    value: number;
+                };
+            }>,
+    )
+    .then((json) => {
+        document.querySelector("article")!.innerHTML +=
+            `<hr/><p>You are the <strong>${ordinal(
+                json.pageviews.value + 1,
+            )}</strong> visitor to this page!</p>`;
+    });
