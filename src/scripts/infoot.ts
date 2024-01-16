6ae2ed5107ff50a46cc1138e90ee08ca0a53489c
Evan Boehs <evan@boehs.org>
2024-01-16 18:00:32 -0500

    remove one request

--- src/scripts/infoot.ts
+++ src/scripts/infoot.ts
@@ -6,44 +6,32 @@ function ordinal(n: number) {
 
 (async () => {
     let root = "{{{this.config.analytics.base}}}/api/";
-    let siteId = "websites/{{{this.config.analytics.id}}}/";
 
     let token = (
         await fetch(`${root}share/{{{this.config.analytics.pub}}}`).then((v) =>
             v.json(),
         )
     ).token as string;
-    let headers: RequestInit = {
-        headers: {
-            "X-Umami-Share-Token": token,
-        },
-    };
     let res = (
-        await fetch(`${root}${siteId}daterange`, headers)
-            .then(
-                (res) =>
-                    res.json() as Promise<{ maxdate: string; mindate: string }>,
-            )
-            .then((r) =>
-                fetch(
-                    `${root}${siteId}stats?` +
-                        // @ts-ignore
-                        new URLSearchParams({
-                            startAt: Date.parse(r.mindate),
-                            endAt: Date.parse(r.maxdate),
-                            url: window.location.pathname,
-                        }),
-                    headers,
-                ),
-            )
-            .then(
-                (res) =>
-                    res.json() as Promise<{
-                        pageviews: {
-                            value: number;
-                        };
-                    }>,
-            )
+        await fetch(
+            `${root}websites/{{{this.config.analytics.id}}}/stats?` +
+                // @ts-ignore
+                new URLSearchParams({
+                    url: window.location.pathname,
+                }),
+            {
+                headers: {
+                    "X-Umami-Share-Token": token,
+                },
+            },
+        ).then(
+            (res) =>
+                res.json() as Promise<{
+                    pageviews: {
+                        value: number;
+                    };
+                }>,
+        )
     ).pageviews.value;
     let el = document.querySelector("article")!;
     el.innerHTML += `<hr/><p>You are the <strong>${ordinal(
