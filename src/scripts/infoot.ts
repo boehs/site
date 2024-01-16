4215c7b7f3e3b9fc8fe489542c0b6a5f8ce7212a
Evan Boehs <evan@boehs.org>
2024-01-16 16:47:17 -0500

    CODING

--- src/scripts/infoot.ts
+++ src/scripts/infoot.ts
@@ -6,39 +6,47 @@ function ordinal(n: number) {
 
 (async () => {
     let root = "{{{this.config.analytics.base}}}/api/";
-    let siteId = "/{{{this.config.analytics.id}}}/";
+    let siteId = "websites/{{{this.config.analytics.id}}}/";
 
-    let token = await fetch(`${root}share/{{{this.config.analytics.pub}}}`)
-        .then((v) => v.json())
-        .then((j) => j.token as string);
-    let h: RequestInit = {
+    let token = (
+        await fetch(`${root}share/{{{this.config.analytics.pub}}}`).then((v) =>
+            v.json(),
+        )
+    ).token as string;
+    let headers: RequestInit = {
         headers: {
             "X-Umami-Share-Token": token,
         },
     };
-    let range = (await fetch(`${root}websites${siteId}daterange`, h).then(
-        (res) => res.json(),
-    )) as { maxdate: string; mindate: string };
     let res = (
-        (await fetch(
-            `${root}websites${siteId}stats?` +
-                // @ts-ignore
-                new URLSearchParams({
-                    startAt: Date.parse(range.mindate),
-                    endAt: Date.parse(range.maxdate),
-                    url: window.location.pathname,
-                }),
-            h,
-        ).then((res) => res.json())) as {
-            pageviews: {
-                value: number;
-            };
-        }
+        await fetch(`${root}${siteId}daterange`, headers)
+            .then(
+                (res) =>
+                    res.json() as Promise<{ maxdate: string; mindate: string }>,
+            )
+            .then((r) =>
+                fetch(
+                    `${root}${siteId}stats?` +
+                        // @ts-ignore
+                        new URLSearchParams({
+                            startAt: Date.parse(r.mindate),
+                            endAt: Date.parse(r.maxdate),
+                            url: window.location.pathname,
+                        }),
+                    headers,
+                ),
+            )
+            .then(
+                (res) =>
+                    res.json() as Promise<{
+                        pageviews: {
+                            value: number;
+                        };
+                    }>,
+            )
     ).pageviews.value;
-    if (res > 0) {
-        let el = document.querySelector("article")!;
-        el.innerHTML += `<hr/><p>You are the <strong>${ordinal(
-            res,
-        )}</strong> visitor to this page!</p>`;
-    }
+    let el = document.querySelector("article")!;
+    el.innerHTML += `<hr/><p>You are the <strong>${ordinal(
+        res + 1,
+    )}</strong> visitor to this page!</p>`;
 })();
