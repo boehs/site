749ed8e37c95dfa4de4fbfcb18cb7f72d7746e2e
Evan Boehs <evan@boehs.org>
2024-02-13 23:40:37 -0500

    capitalise

--- .eleventy.js
+++ .eleventy.js
@@ -63,6 +63,53 @@ export default function (eleventyConfig) {
             )}" n="${n}">${railsEncode(str, n)}</a>`,
     );
 
+    eleventyConfig.addFilter("titleCase", (str) => {
+        str = str.replace(
+            /([^\W_]+[^\s-]*) */g,
+            (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(),
+        );
+
+        // Certain minor words should be left lowercase unless
+        // they are the first or last words in the string
+        let lowers = [
+            "A",
+            "An",
+            "The",
+            "And",
+            "But",
+            "Or",
+            "For",
+            "Nor",
+            "As",
+            "At",
+            "By",
+            "For",
+            "From",
+            "In",
+            "Into",
+            "Near",
+            "Of",
+            "On",
+            "Onto",
+            "To",
+            "With",
+        ];
+        for (const lower of lowers)
+            str = str.replace(new RegExp("\\s" + lower + "\\s", "g"), (txt) =>
+                txt.toLowerCase(),
+            );
+
+        // Certain words such as initialisms or acronyms should be left uppercase
+        let uppers = ["Id", "Tv"];
+        for (const upper of uppers)
+            str = str.replace(
+                new RegExp("\\b" + upper + "\\b", "g"),
+                upper.toUpperCase(),
+            );
+
+        return str;
+    });
+
     eleventyConfig.addFilter("dateString", (date) =>
         date?.toLocaleDateString(),
     );
