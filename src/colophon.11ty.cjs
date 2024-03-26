af3685cc141234bdf09bf54809053b85c1d10722
Evan Boehs <evan@boehs.org>
2024-03-26 08:34:00 -0400

    fix script?

--- src/colophon.11ty.cjs
+++ src/colophon.11ty.cjs
@@ -3,43 +3,45 @@
 // No modularity :(
 
 const EleventyFetch = require("@11ty/eleventy-fetch");
-const terser = require("terser")
+const terser = require("terser");
 
 class Analytics {
-	data() {
-		return {
-			// Writes to "/my-permalink/index.html"
-			permalink: "colophon.js"
-		};
-	}
+    data() {
+        return {
+            // Writes to "/my-permalink/index.html"
+            permalink: "colophon.js",
+        };
+    }
 
-	async render({config}) {
-		try {
-			let url = config.analytics.script;
+    async render({ config }) {
+        try {
+            let url = config.analytics.script;
 
-			const content = await EleventyFetch(url, {
-				duration: "1d",
-				type: "text"
-			});
+            const content = await EleventyFetch(url, {
+                duration: "1d",
+                type: "text",
+            });
 
-			let script = content
-				.replace(/.\(.\+"website-id"\)/,`"${config.analytics.id}"`)
-				.replace(/.=.\(.\+"host-url"\),/,'')
-                .replace(/.\(.\+"domains"\)\|\|/,'')
+            let script = content
+                .replace(/.\(.\+"website-id"\)/, `"${config.analytics.id}"`)
+                .replace(/.=.\(.\+"host-url"\),/, "")
+                .replace(/.\(.\+"domains"\)\|\|/, "")
                 .replace(/"false"!==.\(.\+"auto-track"\)/, "true")
-                .replace(/.\(.\+"do-not-track"\)/, "false")
-				.replace(/\(.\?.\.replace\(\/\\\/\$\/,""\):.\.src\.split\("\/"\)\.slice\(0,-1\)\.join\("\/"\)\)\+"(.*?)"/, `"${config.analytics.base}$1"`)
+                .replace(
+                    /`\${.\?.\.replace\(\/\\\/\$\/.*?}(.*)`/,
+                    `"${config.analytics.base}$1"`,
+                )
                 // now h isn't needed
-                .replace(/.=.\.getAttribute\.bind\(.\),/, "")
-                // also d = u.currentScript; is not needed but it's too annoying to remove
+                .replace(/.=.\.getAttribute\.bind\(.\),/, "");
+            // also d = u.currentScript; is not needed but it's too annoying to remove
 
-			return (await terser.minify(script)).code
-		} catch (e) {
-			return {
-				// my failure fallback data
-			}
-		}
-	};
+            return (await terser.minify(script)).code;
+        } catch (e) {
+            return {
+                // my failure fallback data
+            };
+        }
+    }
 }
 
 module.exports = Analytics;
