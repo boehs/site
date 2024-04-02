fee0a9fa511a3ad5a6f2ec2be0a1e18805aafb48
Evan Boehs <evan@boehs.org>
2024-04-02 09:21:40 -0400

    auto bump goal

--- src/scripts/infoot.ts
+++ src/scripts/infoot.ts
@@ -45,6 +45,14 @@ function ordinal(n: number) {
         )
         .then((json) => {
             document.querySelector("#postlude > p")!.innerHTML +=
-                ` I'm <a href="{{{this.config.donate}}}" data-umami-event="donate" data-umami-event-place="plea">receiving</a> <strong>$${json.receiving.amount}</strong> per week from <strong>${json.npatrons}</strong> patrons, and my goal is <strong>$${json.goal.amount}</strong>. Feel free to <a href="/contact">contact</a> me with any questions or comments :)`;
+                ` I'm <a href="{{{this.config.donate}}}" data-umami-event="donate" data-umami-event-place="plea">receiving</a> <strong>$${
+                    json.receiving.amount
+                }</strong> per week from <strong>${
+                    json.npatrons
+                }</strong> patrons, and my goal is <strong>$${
+                    json.receiving.amount > json.goal.amount
+                        ? Math.round(Number(json.receiving.amount) / 5) * 5 + 5
+                        : json.goal.amount
+                }</strong>. Feel free to <a href="/contact">contact</a> me with any questions or comments :)`;
         });
 })();
