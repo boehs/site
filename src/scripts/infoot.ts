536d8f0a71c53d460143a8bb58619b71f35b414c
Evan Boehs <evan@boehs.org>
2024-04-11 00:22:29 -0400

    o alr

--- src/scripts/infoot.ts
+++ src/scripts/infoot.ts
@@ -45,7 +45,7 @@ function ordinal(n: number) {
         )
         .then((json) => {
             document.querySelector("#postlude > p")!.innerHTML +=
-                ` I'm <a href="{{{this.config.donate}}}" data-umami-event="donate" data-umami-event-place="plea">receiving</a> (<a href="https://ko-fi.com/boehs" data-umami-event="donate" data-umami-event-place="plea-kofi">☕️</a>)
+                ` I'm <a href="{{{this.config.donate}}}" data-umami-event="donate" data-umami-event-place="plea">receiving</a> (<a href="https://ko-fi.com/evan" data-umami-event="donate" data-umami-event-place="plea-kofi">☕️</a>)
                 <strong>$${
                     json.receiving.amount
                 }</strong> per week from <strong>${
