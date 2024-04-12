39cbea5451dec403d741a8f32c2b9656bd20beef
Evan Boehs <evan@boehs.org>
2024-04-11 22:08:57 -0400

    maybe make titles less awful

--- .eleventy.js
+++ .eleventy.js
@@ -102,7 +102,7 @@ export default function (eleventyConfig) {
             );
 
         // Certain words such as initialisms or acronyms should be left uppercase
-        let uppers = ["Id", "Tv", "Css", "Rss", "Xz", "Js", "Html"];
+        let uppers = ["Id", "Tv", "Css", "Rss", "Xz", "Js", "Html", "Llm"];
         for (const upper of uppers)
             str = str.replace(
                 new RegExp("\\b" + upper + "\\b", "g"),
