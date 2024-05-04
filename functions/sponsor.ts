462c9714b73f900d6062bff2b070a5c4bc4d7249
Evan Boehs <evan@boehs.org>
2024-05-03 22:26:16 -0400

    this should cache bette

--- functions/sponsor.ts
+++ functions/sponsor.ts
@@ -7,14 +7,18 @@ export async function onRequest(context: EventContext): PagesFunction {
     } = context;
 
     const response = await next();
-
+    let day = new Date();
     let api = await fetch(
         `${cfg.analytics.base}/api/websites/${cfg.analytics.id}/stats?startAt=${
-            Date.now() - 60 * 60 * 24 * 30 * 1000
-        }&endAt=${Date.now()}`,
+            day.setUTCHours(0, 0, 0, 0) - 60 * 60 * 24 * 30 * 1000
+        }&endAt=${day.setUTCHours(0, 0, 0, 0)}`,
         {
             headers: {
                 "X-Umami-Share-Token": cfg.analytics.pub,
+            },
+            cf: {
+                cacheTtl: 60 * 60 * 60 * 24,
+                cacheEverything: true,
             },
         },
     ).then(
