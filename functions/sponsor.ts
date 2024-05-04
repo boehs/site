9b9c38e529ba58c1c3d23d3a75c22569ac9c0502
Evan Boehs <evan@boehs.org>
2024-05-03 23:01:58 -0400

    one more cache try

--- functions/sponsor.ts
+++ functions/sponsor.ts
@@ -6,39 +6,53 @@ export async function onRequest(context: EventContext): PagesFunction {
         next, // used for middleware or to fetch assets
     } = context;
 
-    const response: Response = await next();
-    response.headers.set(
-        "Cache-Control",
-        "public, max-age=86400, stale-while-revalidate=59",
-    );
-    let day = new Date();
-    let api = await fetch(
-        `${cfg.analytics.base}/api/websites/${cfg.analytics.id}/stats?startAt=${
-            day.setUTCHours(0, 0, 0, 0) - 60 * 60 * 24 * 30 * 1000
-        }&endAt=${day.setUTCHours(0, 0, 0, 0)}`,
-        {
-            headers: {
-                "X-Umami-Share-Token": cfg.analytics.pub,
-            },
-            cf: {
-                cacheTtl: 60 * 60 * 60 * 24,
-                cacheEverything: true,
-            },
-        },
-    ).then(
-        (res) =>
-            res.json() as Promise<{
-                pageviews: {
-                    value: number;
-                };
-            }>,
-    );
+    const cacheUrl = new URL(request.url);
 
-    let res = new HTMLRewriter().on("#viewcount", {
-        element(element) {
-            element.setInnerContent(api.pageviews.value);
-        },
-    });
+    // Construct the cache key from the cache URL
+    const cacheKey = new Request(cacheUrl.toString(), request);
+    const cache = caches.default;
 
-    return res.transform(response);
+    let response = await cache.match(cacheKey);
+
+    if (!response) {
+        response = await next();
+        response.headers.set(
+            "Cache-Control",
+            "public, max-age=86400, stale-while-revalidate=59",
+        );
+        let day = new Date();
+        let api = await fetch(
+            `${cfg.analytics.base}/api/websites/${
+                cfg.analytics.id
+            }/stats?startAt=${
+                day.setUTCHours(0, 0, 0, 0) - 60 * 60 * 24 * 30 * 1000
+            }&endAt=${day.setUTCHours(0, 0, 0, 0)}`,
+            {
+                headers: {
+                    "X-Umami-Share-Token": cfg.analytics.pub,
+                },
+                cf: {
+                    cacheTtl: 60 * 60 * 60 * 24,
+                    cacheEverything: true,
+                },
+            },
+        ).then(
+            (res) =>
+                res.json() as Promise<{
+                    pageviews: {
+                        value: number;
+                    };
+                }>,
+        );
+
+        let res = new HTMLRewriter().on("#viewcount", {
+            element(element) {
+                element.setInnerContent(api.pageviews.value);
+            },
+        });
+
+        context.waitUntil(cache.put(cacheKey, response.clone()));
+
+        return res.transform(response);
+    }
 }
