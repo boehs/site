8c7d4a3b1577dc068e647206f27defada74b763a
Evan Boehs <evan@boehs.org>
2024-05-03 23:24:50 -0400

    maybe maybe

--- functions/sponsor.ts
+++ functions/sponsor.ts
@@ -6,56 +6,47 @@ export async function onRequest(context: EventContext): PagesFunction {
         next, // used for middleware or to fetch assets
     } = context;
 
-    const cacheUrl = new URL(context.request.url);
+    const response = await next();
+    response.headers.set(
+        "Cache-Control",
+        "public, max-age=86400, stale-while-revalidate=59",
+    );
+    let day = new Date();
+    const cacheUrl = new URL(
+        `${cfg.analytics.base}/api/websites/${cfg.analytics.id}/stats?startAt=${
+            day.setUTCHours(0, 0, 0, 0) - 60 * 60 * 24 * 30 * 1000
+        }&endAt=${day.setUTCHours(0, 0, 0, 0)}`,
+    );
 
     // Construct the cache key from the cache URL
     const cacheKey = new Request(cacheUrl.toString(), context.request);
     const cache = caches.default;
 
-    let response = await cache.match(cacheKey);
-
-    if (!response) {
-        response = await next();
-        response.headers.set(
-            "Cache-Control",
-            "public, max-age=86400, stale-while-revalidate=59",
-        );
-        let day = new Date();
-        let api = await fetch(
-            `${cfg.analytics.base}/api/websites/${
-                cfg.analytics.id
-            }/stats?startAt=${
-                day.setUTCHours(0, 0, 0, 0) - 60 * 60 * 24 * 30 * 1000
-            }&endAt=${day.setUTCHours(0, 0, 0, 0)}`,
-            {
-                headers: {
-                    "X-Umami-Share-Token": cfg.analytics.pub,
-                },
-                cf: {
-                    cacheTtl: 60 * 60 * 60 * 24,
-                    cacheEverything: true,
-                },
+    let api = await cache.match(cacheKey);
+    if (!api) {
+        api = await fetch(cacheUrl, {
+            headers: {
+                "X-Umami-Share-Token": cfg.analytics.pub,
             },
-        ).then(
-            (res) =>
-                res.json() as Promise<{
-                    pageviews: {
-                        value: number;
-                    };
-                }>,
-        );
-
-        let res = new HTMLRewriter().on("#viewcount", {
-            element(element) {
-                element.setInnerContent(api.pageviews.value);
+            cf: {
+                cacheTtl: 60 * 60 * 60 * 24,
+                cacheEverything: true,
             },
         });
-
-        let re = res.transform(response);
-        context.waitUntil(cache.put(cacheKey, re.clone()));
-
-        return re;
+        context.waitUntil(cache.put(cacheKey, api));
     }
 
-    return response;
+    const api2: {
+        pageviews: {
+            value: number;
+        };
+    } = await api.json();
+
+    let res = new HTMLRewriter().on("#viewcount", {
+        element(element) {
+            element.setInnerContent(api2.pageviews.value);
+        },
+    });
+
+    return res.transform(response);
 }
