16abcdd32f144689563ffa76c2a858deaf385d80
Evan Boehs <evan@boehs.org>
2025-04-03 18:13:03 -0400

    css improvements

--- conf/templating/scss.js
+++ conf/templating/scss.js
@@ -2,44 +2,44 @@ import * as sass from "sass";
 import postcss from "postcss";
 import path from "path";
 import postcsso from "postcss-csso";
+import postcsssortmediaqueries from "postcss-sort-media-queries";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function scss(eleventyConfig) {
-    eleventyConfig.addTemplateFormats("scss");
-    eleventyConfig.addExtension("scss", {
-        outputFileExtension: "css",
-        compile: async function (inputContent, inputPath) {
-            if (inputPath.split("/").at(-1).startsWith("_")) return;
+	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		compile: async function (inputContent, inputPath) {
+			if (inputPath.split("/").at(-1).startsWith("_")) return;
 
-            let { css, loadedUrls } = sass.compileString(inputContent, {
-                loadPaths: [path.parse(inputPath).dir || "."],
-                sourceMap: false,
-            });
+			const { css, loadedUrls } = sass.compileString(inputContent, {
+				loadPaths: [path.parse(inputPath).dir || "."],
+				sourceMap: false,
+			});
 
-            this.addDependencies(inputPath, loadedUrls);
+			this.addDependencies(inputPath, loadedUrls);
 
-            return async () => {
-                if (process.env.ELEVENTY_ENV === "production") {
-                    const { content } = await postcss([
-                        postcsso({
-                            restructure: true,
-                        }),
-                    ]).process(css, {
-                        from: undefined,
-                    });
+			return async () => {
+				if (process.env.ELEVENTY_ENV === "production") {
+					const { content } = await postcss([
+						postcsssortmediaqueries(),
+						postcsso({
+							restructure: true,
+						}),
+					]).process(css, {
+						from: undefined,
+					});
 
-                    return content;
-                } else {
-                    return css;
-                }
-            };
-        },
-        compileOptions: {
-            permalink: function (contents, inputPath) {
-                return inputPath
-                    .replace("src/styles/", "")
-                    .replace("scss", "css");
-            },
-        },
-    });
+					return content;
+				} else {
+					return css;
+				}
+			};
+		},
+		compileOptions: {
+			permalink: function (contents, inputPath) {
+				return inputPath.replace("src/styles/", "").replace("scss", "css");
+			},
+		},
+	});
 }
