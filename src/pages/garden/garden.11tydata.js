a46747e60ad0b8f81f75fa604676ae4743387b9e
Evan Boehs <evan@boehs.org>
2025-04-04 09:40:57 -0400

    CC BY-SA 4.0 by default

--- src/pages/garden/garden.11tydata.js
+++ src/pages/garden/garden.11tydata.js
@@ -8,100 +8,96 @@ import slugshive from "../../../utils/slugify.js";
 const wikilinkRegExp = /(?<!!)\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g;
 
 function backlinksApi(data) {
-    const notes = data.collections.all;
-    const currentFileSlug = slugshive(
-        data.page.url
-            ? data.page.url.replace("/node/", "").replace(".html", "")
-            : data.page.filePathStem.replace("/pages/garden/node/", ""),
-    );
+	const notes = data.collections.all;
+	const currentFileSlug = slugshive(
+		data.page.url
+			? data.page.url.replace("/node/", "").replace(".html", "")
+			: data.page.filePathStem.replace("/pages/garden/node/", ""),
+	);
 
-    let backlinks = [];
+	let backlinks = [];
 
-    data.internal.exists?.add(currentFileSlug);
+	data.internal.exists?.add(currentFileSlug);
 
-    // Search the other notes for backlinks
-    for (const otherNote of notes) {
-        let noteContent = otherNote.rawInput;
-        if (!noteContent || typeof noteContent !== "string") {
-            continue;
-        }
-        // Remove content within noteContent ``` code blocks
-        noteContent = noteContent.replace(/```[\s\S]*?```/g, "");
+	// Search the other notes for backlinks
+	for (const otherNote of notes) {
+		let noteContent = otherNote.rawInput;
+		if (!noteContent || typeof noteContent !== "string") {
+			continue;
+		}
+		// Remove content within noteContent ``` code blocks
+		noteContent = noteContent.replace(/```[\s\S]*?```/g, "");
 
-        const noteAsLink = slugshive(
-            otherNote.data.page.url
-                ? data.page.url.replace("/node/", "").replace(".html", "")
-                : otherNote.data.page.filePathStem.replace(
-                      "/pages/garden/node/",
-                      "",
-                  ),
-        );
+		const noteAsLink = slugshive(
+			otherNote.data.page.url
+				? data.page.url.replace("/node/", "").replace(".html", "")
+				: otherNote.data.page.filePathStem.replace("/pages/garden/node/", ""),
+		);
 
-        data.internal.exists?.add(noteAsLink);
+		data.internal.exists?.add(noteAsLink);
 
-        // Get all links from otherNote
-        const outboundLinks = (function () {
-            switch (otherNote.data.links) {
-                case undefined: {
-                    const links = (noteContent.match(wikilinkRegExp) || []).map(
-                        (link) =>
-                            // Extract link location
-                            slugshive(link)
-                                .slice(2, -2)
-                                .split("|")[0]
-                                .replace(/.(md|markdown)\s?$/i, "")
-                                .trim()
-                                .toLowerCase(),
-                    );
-                    otherNote.data.links = links;
-                    return links;
-                }
-                default:
-                    return otherNote.data.links;
-            }
-        })();
-        // If the other note links here, return related info
-        if (outboundLinks.some((link) => link === currentFileSlug)) {
-            backlinks.push({
-                url: otherNote.url,
-                title: otherNote.data.title,
-            });
-        }
+		// Get all links from otherNote
+		const outboundLinks = (function () {
+			switch (otherNote.data.links) {
+				case undefined: {
+					const links = (noteContent.match(wikilinkRegExp) || []).map((link) =>
+						// Extract link location
+						slugshive(link)
+							.slice(2, -2)
+							.split("|")[0]
+							.replace(/.(md|markdown)\s?$/i, "")
+							.trim()
+							.toLowerCase(),
+					);
+					otherNote.data.links = links;
+					return links;
+				}
+				default:
+					return otherNote.data.links;
+			}
+		})();
+		// If the other note links here, return related info
+		if (outboundLinks.some((link) => link === currentFileSlug)) {
+			backlinks.push({
+				url: otherNote.url,
+				title: otherNote.data.title,
+			});
+		}
 
-        (otherNote.data.aliases || []).forEach(function (alias) {
-            data.internal.exists?.add(slugshive(alias));
-        });
+		(otherNote.data.aliases || []).forEach(function (alias) {
+			data.internal.exists?.add(slugshive(alias));
+		});
 
-        outboundLinks.forEach((link) => {
-            if (!data.internal.exists.has(link))
-                data.internal.four.add(
-                    data.page.url.replace(/\/[^\/]*?(\..+)?$/, `/${link}$1`),
-                );
-        });
-    }
-    return [backlinks, data.internal.four];
+		outboundLinks.forEach((link) => {
+			if (!data.internal.exists.has(link))
+				data.internal.four.add(
+					data.page.url.replace(/\/[^\/]*?(\..+)?$/, `/${link}$1`),
+				);
+		});
+	}
+	return [backlinks, data.internal.four];
 }
 
 export default {
-    layout: "post.vto",
-    in: "garden",
-    isContentPage: true,
-    structuredData: {
-        type: "BlogPosting",
-    },
-    permalink: (data) => {
-        return (
-            slugify(data.page.filePathStem.replace("/pages/garden/", "/")) +
-            ".html"
-        );
-    },
-    eleventyComputed: {
-        backlinks: (data) => {
-            return backlinksApi(data)[0];
-        },
-        brokenLinks: (data, canRun = false) => {
-            if (!canRun) return "not called from 11ty config";
-            return backlinksApi(data)[1];
-        },
-    },
+	layout: "post.vto",
+	in: "garden",
+	license: "CC BY-SA 4.0",
+	isContentPage: true,
+	structuredData: {
+		type: "BlogPosting",
+	},
+	permalink: (data) => {
+		return (
+			slugify(data.page.filePathStem.replace("/pages/garden/", "/")) + ".html"
+		);
+	},
+	eleventyComputed: {
+		backlinks: (data) => {
+			return backlinksApi(data)[0];
+		},
+		brokenLinks: (data, canRun = false) => {
+			if (!canRun) return "not called from 11ty config";
+			return backlinksApi(data)[1];
+		},
+	},
 };
