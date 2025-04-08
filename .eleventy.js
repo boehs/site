ca2324861410bc3cfd45602664eb540b00cbd901
Evan Boehs <evan@boehs.org>
2025-04-07 22:29:48 -0400

    add pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,7 @@
 import { minify } from "html-minifier";
 
-import { createRequire } from "node:module";
-import { readFileSync } from "fs";
-
-const require = createRequire(import.meta.url);
-const collectionControl = require("./src/_data/collectionsControl.json");
-
 // the files we want to use for our collections
-let gardenStr = "./src/pages/garden/node/**/*.{md,csv}";
+const gardenStr = "./src/pages/garden/node/**/*.{md,csv}";
 
 import slugify from "./utils/slugify.js";
 
@@ -22,6 +16,7 @@ import {
 } from "./conf/components/social/social.js";
 import javascript from "./conf/templating/javascript.js";
 import filters from "./conf/filters.js";
+import { taxes } from "./conf/collections/taxes.js";
 
 Error.stackTraceLimit = 100;
 
@@ -112,146 +107,7 @@ export default function (eleventyConfig) {
 		return redirects;
 	});
 
-	eleventyConfig.addCollection("taxes", function (collectionApi) {
-		// lets make a variable to hold our taxonomies and values
-		let taxAndValues = [];
-		// We need to get each post in our posts folder. In my case this is /node
-		const nodes = collectionApi.getFilteredByGlob(gardenStr);
-		// next lets iterate over all the nodes
-		nodes.forEach((node) => {
-			// and then iterate over the taxonomies
-			for (const [taxonomy, value] of Object.entries(collectionControl)) {
-				// I don't want to paginate date, for instance
-				// this is why my collectionControl is using objects instead of arrays
-				// @ts-ignore
-				if (value.excludeFromPagination) continue;
-				else if (node?.data?.[taxonomy]) {
-					// this is typeof on drugs
-					switch (
-						{}.toString
-							.call(node.data[taxonomy])
-							.match(/\s([a-zA-Z]+)/)[1]
-							.toLowerCase()
-					) {
-						// if it is an array (for tags especially)
-						case "array":
-							node.data[taxonomy].forEach((item) => {
-								taxAndValues.push([taxonomy, item]);
-							});
-							break;
-						// otherwise
-						default:
-							taxAndValues.push([taxonomy, node.data[taxonomy]]);
-					}
-				}
-			}
-		});
-
-		// custom set, sets don't work with objects
-		// @ts-ignore
-		const unique = [...new Set(taxAndValues.map(JSON.stringify))].map(
-			// @ts-ignore
-			JSON.parse,
-		);
-
-		return unique;
-	});
-
-	eleventyConfig.addCollection("taxesDiffer", function (collectionApi) {
-		let taxAndValues = [];
-		const nodes = collectionApi.getFilteredByGlob(gardenStr);
-		const differ =
-			Object.keys(collectionControl)[
-				Object.values(collectionControl).findIndex(
-					// @ts-ignore
-					(value) => value.mode == "differentiator",
-				)
-			];
-		nodes.forEach((node) => {
-			for (const [taxonomy, value] of Object.entries(collectionControl)) {
-				// @ts-ignore
-				if (value.excludeFromPagination) continue;
-				else if (node?.data?.[taxonomy]) {
-					switch (
-						{}.toString
-							.call(node.data[taxonomy])
-							.match(/\s([a-zA-Z]+)/)[1]
-							.toLowerCase()
-					) {
-						case "array": {
-							node.data[taxonomy].forEach((item) => {
-								if (!(taxonomy == differ && item == node.data[differ]))
-									taxAndValues.push([taxonomy, item, node.data[differ]]);
-							});
-							break;
-						}
-						default:
-							if (
-								!(
-									taxonomy == differ && node.data[taxonomy] == node.data[differ]
-								)
-							)
-								taxAndValues.push([
-									taxonomy,
-									node.data[taxonomy],
-									node.data[differ],
-								]);
-					}
-				}
-			}
-		});
-
-		// custom set, sets don't work with objects
-		// @ts-ignore
-		const unique = [...new Set(taxAndValues.map(JSON.stringify))].map(
-			// @ts-ignore
-			JSON.parse,
-		);
-
-		return unique;
-	});
-
-	eleventyConfig.addCollection("nestedTax", function (collectionApi) {
-		let nestedTax = {};
-		const nodes = collectionApi.getFilteredByGlob(gardenStr);
-		nodes.forEach((node) => {
-			for (const [taxonomy, value] of Object.entries(collectionControl)) {
-				const taxValue = node.data[taxonomy];
-
-				// @ts-ignore
-				if (value.excludeFromPagination) continue;
-				else if (node?.data?.[taxonomy]) {
-					if (!nestedTax[taxonomy]) nestedTax[taxonomy] = {};
-					switch (
-						{}.toString
-							.call(taxValue)
-							.match(/\s([a-zA-Z]+)/)[1]
-							.toLowerCase()
-					) {
-						case "array": {
-							taxValue.forEach((item) => {
-								// if the value in the object does not yet exist
-								if (!nestedTax[taxonomy][item]) nestedTax[taxonomy][item] = [];
-								// then add the entire page to it
-								nestedTax[taxonomy][item].push(node);
-							});
-							break;
-						}
-						// otherwise
-						default: {
-							// if the value in the object does not yet exist
-							if (!nestedTax[taxonomy][taxValue])
-								nestedTax[taxonomy][taxValue] = [];
-							// then add the entire page to it
-							nestedTax[taxonomy][taxValue].push(node);
-						}
-					}
-				}
-			}
-		});
-
-		return nestedTax;
-	});
+	taxes(eleventyConfig);
 
 	javascript(eleventyConfig);
 	scss(eleventyConfig);
---
+++ conf/collections/taxes.js
@@ -0,0 +1,203 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const collectionControl = require("../../src/_data/collectionsControl.json");
+const gardenStr = "./src/pages/garden/node/**/*.{md,csv}";
+
+// this is typeof on drugs
+function getType(value) {
+	return {}.toString
+		.call(value)
+		.match(/\s([a-zA-Z]+)/)[1]
+		.toLowerCase();
+}
+
+function standard(collectionApi) {
+	// lets make a variable to hold our taxonomies and values
+	const taxAndValues = {};
+	// We need to get each post in our posts folder. In my case this is /node
+	const nodes = collectionApi.getFilteredByGlob(gardenStr);
+	// next lets iterate over all the nodes
+	for (const node of nodes) {
+		// and then iterate over the taxonomies
+		for (const [taxonomy, value] of Object.entries(collectionControl)) {
+			// I don't want to paginate date, for instance
+			// this is why my collectionControl is using objects instead of arrays
+			// @ts-expect-error
+			if (value.excludeFromPagination) continue;
+			if (node?.data?.[taxonomy]) {
+				switch (getType(node.data[taxonomy])) {
+					// if it is an array (for tags especially)
+					case "array":
+						for (const item of node.data[taxonomy]) {
+							if (!taxAndValues[`${taxonomy}/./${item}`])
+								taxAndValues[`${taxonomy}/./${item}`] = 0;
+							taxAndValues[`${taxonomy}/./${item}`] += 1;
+						}
+						break;
+					// otherwise
+					default:
+						if (!taxAndValues[`${taxonomy}/./${node.data[taxonomy]}`])
+							taxAndValues[`${taxonomy}/./${node.data[taxonomy]}`] = 0;
+						taxAndValues[`${taxonomy}/./${node.data[taxonomy]}`] += 1;
+				}
+			}
+		}
+	}
+	return taxAndValues;
+}
+
+/** @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig */
+export function taxes(eleventyConfig) {
+	// biome-ignore lint/complexity/useArrowFunction: <explanation>
+	eleventyConfig.addCollection("taxes", function (collectionApi) {
+		const taxAndValues = standard(collectionApi);
+
+		// @ts-ignore
+		const unique = Object.keys(taxAndValues).map((key) => {
+			const [taxonomy, value] = key.split("/./");
+			return [taxonomy, value];
+		});
+
+		return unique;
+	});
+
+	// biome-ignore lint/complexity/useArrowFunction: <explanation>
+	eleventyConfig.addCollection("taxesPaged", function (collectionApi) {
+		const taxAndValues = standard(collectionApi);
+
+		const paginatedResults = [];
+		const maxItemsPerPage = 30;
+		const minItemsForLastPage = 5;
+
+		for (const key of Object.keys(taxAndValues)) {
+			const [taxonomy, value] = key.split("/./");
+			let totalItems = taxAndValues[key];
+			let pageNumber = 0;
+
+			if (collectionControl[taxonomy].mode !== "differentiator") {
+				paginatedResults.push({
+					t: taxonomy,
+					v: value,
+					n: pageNumber,
+					itemsInPage: totalItems,
+					hasNext: false,
+					hasPrev: false,
+				});
+				continue;
+			}
+
+			while (totalItems > 0) {
+				let itemsInPage = Math.min(totalItems, maxItemsPerPage);
+
+				if (
+					totalItems - itemsInPage < minItemsForLastPage &&
+					totalItems > maxItemsPerPage
+				) {
+					itemsInPage += totalItems - itemsInPage;
+				}
+
+				paginatedResults.push({
+					t: taxonomy,
+					v: value,
+					n: pageNumber,
+					itemsInPage: itemsInPage,
+					hasNext: totalItems > itemsInPage,
+					hasPrev: pageNumber > 0,
+				});
+
+				totalItems -= itemsInPage;
+				pageNumber++;
+			}
+		}
+
+		return paginatedResults;
+	});
+
+	// biome-ignore lint/complexity/useArrowFunction: <explanation>
+	eleventyConfig.addCollection("taxesDiffer", function (collectionApi) {
+		const taxAndValues = [];
+		const nodes = collectionApi.getFilteredByGlob(gardenStr);
+		const differ =
+			Object.keys(collectionControl)[
+				Object.values(collectionControl).findIndex(
+					// @ts-expect-error
+					(value) => value.mode === "differentiator",
+				)
+			];
+		for (const node of nodes) {
+			for (const [taxonomy, value] of Object.entries(collectionControl)) {
+				// @ts-ignore
+				if (value.excludeFromPagination) continue;
+				if (node?.data?.[taxonomy]) {
+					switch (getType(node.data[taxonomy])) {
+						case "array": {
+							for (const item of node.data[taxonomy]) {
+								if (!(taxonomy == differ && item == node.data[differ]))
+									taxAndValues.push([taxonomy, item, node.data[differ]]);
+							}
+							break;
+						}
+						default:
+							if (
+								!(
+									taxonomy == differ && node.data[taxonomy] == node.data[differ]
+								)
+							)
+								taxAndValues.push([
+									taxonomy,
+									node.data[taxonomy],
+									node.data[differ],
+								]);
+					}
+				}
+			}
+		}
+
+		// custom set, sets don't work with objects
+		// @ts-ignore
+		const unique = [...new Set(taxAndValues.map(JSON.stringify))].map(
+			// @ts-ignore
+			JSON.parse,
+		);
+
+		return unique;
+	});
+
+	// biome-ignore lint/complexity/useArrowFunction: <explanation>
+	eleventyConfig.addCollection("nestedTax", function (collectionApi) {
+		const nestedTax = {};
+		const nodes = collectionApi.getFilteredByGlob(gardenStr);
+		for (const node of nodes) {
+			for (const [taxonomy, value] of Object.entries(collectionControl)) {
+				const taxValue = node.data[taxonomy];
+
+				// @ts-expect-error
+				if (value.excludeFromPagination) continue;
+				if (node?.data?.[taxonomy]) {
+					if (!nestedTax[taxonomy]) nestedTax[taxonomy] = {};
+					switch (getType(taxValue)) {
+						case "array": {
+							for (const item of taxValue) {
+								// if the value in the object does not yet exist
+								if (!nestedTax[taxonomy][item]) nestedTax[taxonomy][item] = [];
+								// then add the entire page to it
+								nestedTax[taxonomy][item].push(node);
+							}
+							break;
+						}
+						// otherwise
+						default: {
+							// if the value in the object does not yet exist
+							if (!nestedTax[taxonomy][taxValue])
+								nestedTax[taxonomy][taxValue] = [];
+							// then add the entire page to it
+							nestedTax[taxonomy][taxValue].push(node);
+						}
+					}
+				}
+			}
+		}
+
+		return nestedTax;
+	});
+}
