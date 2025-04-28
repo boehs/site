e8cefee558e3d36262e90f9f8ffc380d16946cb7
Evan Boehs <evan@boehs.org>
2025-04-28 14:15:00 -0400

    misc

--- conf/templating/markdown/widgets/widgets.js
+++ conf/templating/markdown/widgets/widgets.js
@@ -2,32 +2,32 @@ import renderPintora from "./pintora/index.js";
 import exec from "./eval.js";
 
 const widgets = {
-    PINTORA: renderPintora,
-    EVAL: exec,
+	PINTORA: renderPintora,
+	EVAL: exec,
 };
 
 export async function inject(result) {
-    let foundWidgets = [];
-    const regex = /___(\w{4,})_(\w+)_(.*?)_/gs;
-    let match;
-    while ((match = regex.exec(result)) !== null) {
-        foundWidgets.push({
-            mode: match[1],
-            id: match[2],
-            content: match[3],
-        });
-    }
-    for (const block of foundWidgets) {
-        let map = widgets[block.mode];
-        if (!map) {
-            console.error(`Unknown widget type: ${block.mode}`);
-            continue;
-        }
-        const rendered = await map(block.content);
-        result = result.replace(
-            `___${block.mode}_${block.id}_${block.content}_`,
-            rendered,
-        );
-    }
-    return result;
+	let foundWidgets = [];
+	const regex = /___(\w{4,})_(\w+)_(.*?)___/gs;
+	let match;
+	while ((match = regex.exec(result)) !== null) {
+		foundWidgets.push({
+			mode: match[1],
+			id: match[2],
+			content: match[3],
+		});
+	}
+	for (const block of foundWidgets) {
+		let map = widgets[block.mode];
+		if (!map) {
+			console.error(`Unknown widget type: ${block.mode}`);
+			continue;
+		}
+		const rendered = await map(block.content);
+		result = result.replace(
+			`___${block.mode}_${block.id}_${block.content}___`,
+			rendered,
+		);
+	}
+	return result;
 }
